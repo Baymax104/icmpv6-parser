3e99e7e8d3bedc9ccd60423ccbba827ba7e3371f
Baymax104 <wzy1048168235@163.com>
2024-03-19 18:00:17 +0800

    添加项目文件。

---
+++ Test/Program.cs
@@ -0,0 +1,11 @@
+﻿using SharpPcap;
+using PacketDotNet;
+using System;
+
+namespace Test {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Test/model/NetPacket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.model {
+    internal abstract class NetPacket(ByteSegment packet) {
+
+        protected ByteSegment Packet { get; } = packet;
+
+        protected ByteSegment Header { get; } = packet;
+
+        protected ByteSegment Payload { get; } = packet;
+
+        public NetPacket(byte[] packet) : this(new ByteSegment(packet)) { }
+
+
+    }
+}
